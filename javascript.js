ed052cb8b765120b5a3987543e6d98e16764d0b1
Ryan <ryan.irani@gmail.com>
2024-02-16 22:27:43 +0000

    Javascript completed. User interface still requires some work.

---
+++ javascript.js
@@ -0,0 +1,66 @@
+
+const sketchpad = document.getElementById("container");
+const userColour = document.getElementById("color");
+const resize = document.getElementById("resizeBtn");
+const clear = document.getElementById("clear");
+let divElements = [];
+let GridSize = 500;
+let rows = 16;
+let cols = 16;
+
+sketchpad.style.width = `${GridSize}px`;
+sketchpad.style.height = `${GridSize}px`;
+
+function gridSize(){
+    let result = prompt("Please enter the size that you would like the grid");
+    while (result <= 1 || result >=101){
+        alert("Please enter a value between 2 and 100");
+        result = prompt("Please enter the size that you would like the grid");
+    }
+        rows = parseInt(result);
+        cols = parseInt(result);
+        clearGrid(); 
+        createGridCells();
+        selectColour();
+    }
+ 
+
+ function clearGrid() {
+    divElements.forEach(div => {
+        sketchpad.removeChild(div);
+    });
+    divElements = [];
+}
+
+function createGridCells(){
+    for (i = 0; i < (rows * cols); i++){
+        const div = document.createElement("div");
+        div.style.width = `${(GridSize / rows) - 2}px`;
+        div.style.height = `${(GridSize / cols) - 2}px`;
+        div.classList.add("cell")
+        sketchpad.appendChild(div);
+        divElements.push(div);   
+    }
+    
+}
+
+function selectColour(){
+    divElements.forEach(div => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = userColour.value;
+        })
+    })
+
+    }
+
+ 
+resize.addEventListener("click", gridSize)
+clear.addEventListener("click", () => {
+    clearGrid();
+    createGridCells();
+    selectColour();
+})
+
+createGridCells()
+selectColour()
+gridSize()
