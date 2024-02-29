a9143978df4d5458e0d755e0c0b79b429c553a37
Ryan <ryan.irani@gmail.com>
2024-02-29 11:01:11 +1100

    Added save/load feature. Bug that unable to edit once loaded

--- javascript.js
+++ javascript.js
@@ -5,12 +5,14 @@ const clear = document.getElementById("clear");
 const undoBtn = document.getElementById("undo");
 const save = document.getElementById("save");
 const load = document.getElementById("load");
+const text = document.getElementById("text");
 
 let divElements = [];
 let gridStates = []; 
 let GridSize = 500;
 let rows = 16;
 let cols = 16;
+let mouseIsDown = false;
 
 sketchpad.style.width = `${GridSize}px`;
 sketchpad.style.height = `${GridSize}px`;
@@ -27,12 +29,12 @@ function gridSize(){
             alert("Please enter a value between 2 and 100");
             result = prompt("Please enter the size that you would like the grid");
     }
-}
+    }
     rows = parseInt(result);
     cols = parseInt(result);
     clearGrid(); 
     createGridCells();
-    selectColour();
+    draw();
     gridStates = [];
 }
  
@@ -42,6 +44,7 @@ function clearGrid() {
     });
     divElements = [];
     gridStates = [];
+    text.innerText = "";
 }
 
 function createGridCells(){
@@ -49,27 +52,10 @@ function createGridCells(){
         const div = document.createElement("div");
         div.style.width = `${(GridSize / rows) - 2}px`;
         div.style.height = `${(GridSize / cols) - 2}px`;
-        div.classList.add("cell")
+        div.classList.add("cell");
         sketchpad.appendChild(div);
         divElements.push(div);   
     }
-}
-
-let mouseIsDown = false;
-sketchpad.addEventListener('mousedown', () => {
-    mouseIsDown = true;
-});
-
-sketchpad.addEventListener('mouseup', () => {
-    mouseIsDown = false;
-});
-
-function captureGridState() {
-    const currentState = [];
-    divElements.forEach(div => {
-        currentState.push(div.style.backgroundColor);
-    });
-    gridStates.push(currentState);
 }
 
 function undo() {
@@ -82,66 +68,73 @@ function undo() {
     }
 }
 
-undoBtn.addEventListener("click", undo);
-
-function selectColour(){
-    divElements.forEach(div => {
-        div.addEventListener("mouseover", () => {
-            if (mouseIsDown) {
-                captureGridState();
-                div.style.backgroundColor = userColour.value;
-            }
-        })
-
-        div.addEventListener("mousedown", () => {
-            captureGridState();
-            div.style.backgroundColor = userColour.value;
-        })
-    })
-}
-
-resize.addEventListener("click", gridSize)
-clear.addEventListener("click", () => {
-    clearGrid();
-    createGridCells();
-    selectColour();
-})
-
-createGridCells()
-selectColour()
-gridSize()
-
-
 function saveData(){
     if (typeof(Storage) !== "undefined"){
-        localStorage.setItem('savedDrawing', JSON.stringify(divElements));
+        const markedCells = divElements.map(div => div.style.backgroundColor !== "" ? true : false);
+        localStorage.setItem('markedCells', JSON.stringify(markedCells));
         localStorage.setItem('savedCols', JSON.stringify(cols));
         localStorage.setItem('savedRows', JSON.stringify(rows));
-        alert('Data saved successfully');
+        text.innerText = 'Data saved successfully';
     } else {
-        alert('Local storage is not supported in this browser.')
+        text.innerText = 'Local storage is not supported in this browser.';
     }
 }
 
 function loadData(){
     console.log("Loading data...");
-    if (localStorage.getItem('savedDrawing') !== null){
-        const savedDivs = JSON.parse(localStorage.getItem('savedDrawing'));
+    if (localStorage.getItem('markedCells') !== null){
+        const markedCells = JSON.parse(localStorage.getItem('markedCells'));
         const savedCols = JSON.parse(localStorage.getItem('savedCols'));
         const savedRows = JSON.parse(localStorage.getItem('savedRows'));
-        
         clearGrid();
         cols = savedCols;
         rows = savedRows;
         createGridCells();
 
-        savedDivs.forEach((color, index) => {
-            divElements[index].style.backgroundColor = color;
-        })
+        markedCells.forEach((marked, index) => {
+            if (marked) {
+                divElements[index].style.backgroundColor = userColour.value;
+            }
+        });
         
     }
 
 }
 
+function draw(){
+    divElements.forEach(div => {
+        div.addEventListener("mouseover", () => {
+            if (mouseIsDown) {
+                div.style.backgroundColor = userColour.value;
+            }
+        })
+        div.addEventListener("mousedown", () => {
+            div.style.backgroundColor = userColour.value;
+        })
+    })
+}
+
+sketchpad.addEventListener('mousedown', () => {
+    mouseIsDown = true;
+});
+
+sketchpad.addEventListener('mouseup', () => {
+    mouseIsDown = false;
+});
+
+resize.addEventListener("click", gridSize);
+
+clear.addEventListener("click", () => {
+    clearGrid();
+    createGridCells();
+    draw();
+})
+
+undoBtn.addEventListener("click", undo);
 save.addEventListener('click', saveData);
 load.addEventListener('click', loadData);
+
+createGridCells()
+draw()
+gridSize()
+
